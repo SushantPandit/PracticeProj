08ceb9f56f47d3d6d5376bf774de8c9ad5189ffd
wtt161 <wtt161@localhost.localdomain>
2018-11-08 19:58:22 +0530

    display a list completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material'
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormControl } from '@angular/forms/src/model';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent
   ],
+  
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    ReactiveFormsModule
+    
+
   ],
+  exports: [MatButtonModule,
+    MatCheckboxModule,
+    ReactiveFormsModule
+  ],
+
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
